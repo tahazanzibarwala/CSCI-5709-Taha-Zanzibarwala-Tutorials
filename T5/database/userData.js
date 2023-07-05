6cd335c042f80480919e94cd648ff400fbbfb74f
Taha Mustufa Zanzibarwala <th803624@dal.ca>
2023-07-05 19:49:29 -0300

    Upload New File

---
+++ T5/database/userData.js
@@ -0,0 +1,19 @@
+const userData = [
+    {
+        email: 'th803624@dal.ca',
+        id: '1',
+        firstName: 'Taha Zanzibarwala'
+    },
+    {
+        email: 'jane@dal.ca',
+        id: '2',
+        firstName: 'Jane Smith'
+    },
+    {
+        email: 'john@dal.ca',
+        id: '3',
+        firstName: 'John Doe'
+    },
+];
+
+module.exports = userData;
